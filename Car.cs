567d38192bd4e7b12fb66f014455fcd21b6bd0c4
PrettyKawaii <sasha.litv.inovich@bk.ru>
2019-08-04 10:39:37 +0300

    v0.1 Added class car;

---
+++ Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    class Car
+    {
+        string mark;
+        string model;
+        DateTime productionYear;
+        string color;
+        int kmTraveled;
+    }
+}
