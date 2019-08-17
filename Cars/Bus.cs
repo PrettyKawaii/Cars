5912889348209fe8f9e12781f015d734cdac2cca
PrettyKawaii <sasha.litv.inovich@bk.ru>
2019-08-17 13:00:19 +0300

    v1.4!
    Added doorTurn and their methods!

--- Cars/Bus.cs
+++ Cars/Bus.cs
@@ -8,7 +8,36 @@ namespace Cars
 {
     class Bus:Car
     {
-        public bool doorStatus;
+        public Bus(int seatingNum)
+        {
+            seatingNum = 40;
+        }
+        public static bool doorStatus;
         public int seatingNum;
+
+        public static void doorTurn()
+        {
+            Console.Write("1 - open door; 2 - close door");
+            int selection = Int32.Parse(Console.ReadLine());
+            switch(selection)
+            {
+                case 1:
+                    doorOn();
+                    break;
+                case 2:
+                    doorOff();
+                    break;
+            }
+        }
+        public static void doorOn()
+        {
+            doorStatus = true;
+            Console.WriteLine("Door was turned on!");
+        }
+        public static void doorOff()
+        {
+            doorStatus = false;
+            Console.WriteLine("Door was turned off!");
+        }
     }
 }
